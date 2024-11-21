24f811956e1c7f16f4350221fd0bd31cd416f6ab
CHOEJAEHYEOK <lincoln1604@gmail.com>
2024-11-21 11:45:44 +0900

    new commit

---
+++ Java_Study/src/chap15/ImageDemo.java
@@ -0,0 +1,41 @@
+package chap15;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ImageDemo extends JFrame{
+	ImageDemo(){
+		setTitle("이미지 그리기");
+		
+		class MyPanel extends JPanel{
+			BufferedImage img;
+			
+			public MyPanel() {
+				try {
+					img = ImageIO.read(new File("c:/images/ballons.png"));
+				}catch (IOException e) {
+			}
+		}
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+				
+			g.drawImage(img, 0, 0, null);		
+			}
+	}
+	add(new MyPanel());
+	
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(320, 262);
+	setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ImageDemo();
+	}
+}
